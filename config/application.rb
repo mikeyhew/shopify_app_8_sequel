5c407b9eb49eb2191a62c2009908c21bb414f95d
Michael Hewson <michael@michaelhewson.ca>
2017-09-26 15:52:49 -0400

    remove X-Frame-Options

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module ShopifyApp8Sequel
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_dispatch.default_headers.delete('X-Frame-Options')
+    config.action_dispatch.default_headers['P3P'] = 'CP=\"Not used\"'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
